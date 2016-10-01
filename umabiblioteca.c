08068369cb25791b9f573afa9ed0ba2326911aeb
Higor Alves <Goodyearalves@gmail.com>
2016-10-01 18:53:36 -0300

    Função para verificar os limites do estacionamento

--- umabiblioteca.c
+++ umabiblioteca.c
@@ -12,6 +12,27 @@ void verifica_carros(_veiculo *carros, int *n){
         }
       }
     }
+    if (strcmp(carros[i].paralelo, "Y") == 0){
+      if (strcmp(carros[i].tipo, "2") == 0){
+        if (atoi(carros[i].inicio_y) + 1 > 6 || atoi(carros[i].inicio_y) > 6){
+          printf("Carro ultrapassando o limite do estacionamento\n");
+        }
+      }else if (strcmp(carros[i].tipo, "3") == 0){
+        if (atoi(carros[i].inicio_y) + 2 > 6 || atoi(carros[i].inicio_y) > 6){
+          printf("Carro ultrapassando o limite do estacionamento\n");
+        }
+      }
+    }else{
+      if (strcmp(carros[i].tipo, "2") == 0){
+        if (atoi(carros[i].inicio_x) + 1 > 6 || atoi(carros[i].inicio_x) > 6){
+          printf("Carro ultrapassando o limite do estacionamento\n");
+        }
+      }else if (strcmp(carros[i].tipo, "3") == 0){
+        if (atoi(carros[i].inicio_x) + 2 > 6 || atoi(carros[i].inicio_x) > 6){
+          printf("Carro ultrapassando o limite do estacionamento\n");
+        }
+      }
+    }
   }
 
 }
@@ -19,10 +40,26 @@ void verifica_carros(_veiculo *carros, int *n){
 int prencher_matriz(_veiculo *carros, int *n){
   int matriz[7][7], i = 0, j = 0, aux = 1, aux2 = 0, aux3 = 0;
 
+  /* Zerando a matriz */
+  for (i = 0; i <= 7; i++){
+    for (j = 0; j <= 7; j++){
+      matriz[i][j] = 0;
+    }
+  }
+
+  /* prenchendo a matriz vazia */
   for (aux = 1; aux <= *n; aux++){
     i = atoi(carros[aux].inicio_x);
     j = atoi(carros[aux].inicio_y);
     matriz[i][j] = 33;
+    if (strcmp (carros[aux].paralelo, "Y") == 0){
+      if (strcmp (carros[aux].tipo, "2") == 0){
+        matriz[i+1][j] = 33;
+      }else if (strcmp (carros[aux].tipo, "3") == 0){
+        matriz[i][j+1] = 33;
+        matriz[i][j+2] = 33;
+      }
+    }
   }
   return 0;
 }
