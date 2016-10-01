63730efedd880ced0fe3969899659ee7692e3e74
Higor Alves <Goodyearalves@gmail.com>
2016-10-01 17:45:28 -0300

    Funções adicionadas
    Funçao para verificar se ha carros iguais adicionada

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 
 int main (){
   char word [2]; //Somente para guardar o lixo do arquivo
-  int i = 1, n = 0, j = 1;
+  int i = 1, n = 0, aux = 0;
   /*---------------------------LEITURA ARQUIVO-------------------------*/
   /* Abrimos o arquivo e verificamos se realmente conseguimos abri-lo */
   FILE *IN_carros = fopen ("carros.txt", "r");
@@ -23,19 +23,35 @@ int main (){
   /* Declaramos a quantidade de carros */
   _veiculo carros[n];
 
-  /* Pegar os dados do arquivo */
+  /* Pegar os dados do arquivo e prencher toda a struct*/
   while ((fscanf (IN_carros, "%s %s %s %c%c%c%c", carros[i].nome, carros[i].tipo, carros[i].paralelo, word, carros[i].inicio_x, word, carros[i].inicio_y)) != EOF){
+    if (strcmp (carros[i].paralelo, "Y") == 0){
+      if (strcmp (carros[i].tipo, "2") == 0){
+        carros[i].fim_y = atoi(carros[i].inicio_y) + 2;
+        carros[i].meio = carros[i].fim_y - 1;
+      }else if (strcmp (carros[i].tipo, "3") == 0){
+        carros[i].fim_y = atoi(carros[i].inicio_y) + 2;
+        carros[i].meio = carros[i].fim_y - 1;
+      }else{
+        printf("Tipo do carro é invalido\n");
+      }
+    }else if (strcmp (carros[i].tipo, "X") == 0){
+      if (strcmp (carros[i].tipo, "2") == 0){
+        carros[i].fim_x = atoi(carros[i].inicio_x) + 2;
+        carros[i].meio = carros[i].fim_x - 1;
+      }else if (strcmp (carros[i].tipo, "3") == 0){
+        carros[i].fim_x = atoi(carros[i].inicio_x) + 2;
+        carros[i].meio = carros[i].fim_x - 1;
+      }else{
+        printf("Tipo do carro é invalido\n");
+      }
+    }
     i++;
   }
   fclose(IN_carros);
   /*---------------------------------------------------------------------*/
+  verifica_carros(&carros, &n);
+  prencher_matriz(&carros);
 
-  FILE *IN_manobras = fopen("manabras.txt", "r");
-
-  if (IN_carros == NULL){
-    printf("Não foi possivel ler o arquivo de configuração dos carros\n");
-    return 1;
-  }
-  
   return 0;
 }
--- umabiblioteca.c
+++ umabiblioteca.c
@@ -1,1 +1,26 @@
+#include "umabiblioteca.h"
 
+void verifica_carros(_veiculo *carros, int *n){
+  int i, j;
+
+  for (i = 1; i <= *n; i++){
+    for (j = 1; j <= i; j++){
+      if (strcmp (carros[i].nome, carros[j].nome) == 0 || strcmp (carros[i].inicio_x, carros[j].inicio_y) == 0){
+        if (i != j){
+          printf("Dois carros com mesmo valor verifique o seu arquivo!\n");
+          return 1;
+        }
+      }
+    }
+  }
+
+}
+
+int prencher_matriz(_veiculo *carros){
+  int matriz[5][5], i = 2, j = 0;
+  for (i = 0; i <= 5; i++){
+    for (j = 0; j <= 5; i++){
+      
+    }
+  }
+}
